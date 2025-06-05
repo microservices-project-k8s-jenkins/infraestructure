eb0b8c3d59d8b0819ffed9ff4e458461d6f5f715
Santiago0212004 <sjbs0212@gmail.com>
2025-06-05 00:31:25 -0500

    feat: change IAM role

--- aws-terraform/main.tf
+++ aws-terraform/main.tf
@@ -1,4 +1,9 @@
 data "aws_availability_zones" "available" {}
+data "aws_caller_identity" "current" {}
+
+locals {
+  lab_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
+}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
@@ -11,80 +16,21 @@ resource "aws_subnet" "subnet" {
   availability_zone = data.aws_availability_zones.available.names[count.index]
 }
 
-resource "aws_iam_role" "eks_cluster" {
-  name = "${var.eks_cluster_name}-eks-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
-  role       = aws_iam_role.eks_cluster.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSServicePolicy" {
-  role       = aws_iam_role.eks_cluster.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-}
-
 resource "aws_eks_cluster" "eks" {
   name     = var.eks_cluster_name
-  role_arn = aws_iam_role.eks_cluster.arn
+  role_arn = local.lab_role_arn
 
   vpc_config {
     subnet_ids = aws_subnet.subnet[*].id
   }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSServicePolicy
-  ]
-}
-
-resource "aws_iam_role" "eks_nodes" {
-  name = "${var.eks_cluster_name}-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "worker_node_AmazonEKSWorkerNodePolicy" {
-  role       = aws_iam_role.eks_nodes.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "worker_node_AmazonEKS_CNI_Policy" {
-  role       = aws_iam_role.eks_nodes.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "worker_node_AmazonEC2ContainerRegistryReadOnly" {
-  role       = aws_iam_role.eks_nodes.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "default"
-  node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = aws_subnet.subnet[*].id
+  node_role_arn   = local.lab_role_arn
+
+  subnet_ids = aws_subnet.subnet[*].id
 
   scaling_config {
     desired_size = 1
@@ -93,12 +39,6 @@ resource "aws_eks_node_group" "node_group" {
   }
 
   instance_types = ["t3.xlarge"]
-
-  depends_on = [
-    aws_iam_role_policy_attachment.worker_node_AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.worker_node_AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.worker_node_AmazonEC2ContainerRegistryReadOnly
-  ]
 }
 
 resource "aws_ecr_repository" "ecr" {
