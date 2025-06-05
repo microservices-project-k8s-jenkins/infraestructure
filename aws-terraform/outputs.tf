b592f3ed7f83aad7d4f17c95ada083a80cb4086e
Santiago0212004 <sjbs0212@gmail.com>
2025-06-05 10:17:22 -0500

    fix: delete secret arn

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -6,10 +6,6 @@ output "ecr_repository_url" {
   value = aws_ecr_repository.ecr.repository_url
 }
 
-output "secret_arn" {
-  value = aws_secretsmanager_secret.secret.arn
-}
-
 output "region" {
   value = var.region
 }
