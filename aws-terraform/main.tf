28f668791967b7036da327e4a9cc187cd6551294
Santiago0212004 <sjbs0212@gmail.com>
2025-06-09 21:49:15 -0500

    feat: add vpc cni addon

--- aws-terraform/main.tf
+++ aws-terraform/main.tf
@@ -143,8 +143,8 @@ resource "aws_eks_node_group" "node_group" {
   subnet_ids      = aws_subnet.subnet[*].id
 
   scaling_config {
-    desired_size = 1
-    max_size     = 3
+    desired_size = 2
+    max_size     = 4
     min_size     = 1
   }
 
