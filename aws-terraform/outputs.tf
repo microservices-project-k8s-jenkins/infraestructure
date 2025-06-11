837641464647b44dbf622a5b7873cc5deb42a297
Santiago0212004 <sjbs0212@gmail.com>
2025-06-10 19:56:57 -0500

    fix: delete ecr from infra

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -2,10 +2,6 @@ output "eks_cluster_name" {
   value = aws_eks_cluster.eks.name
 }
 
-output "ecr_repository_url" {
-  value = aws_ecr_repository.ecr.repository_url
-}
-
 output "region" {
   value = var.region
 }
--- aws-terraform/variables.tf
+++ aws-terraform/variables.tf
@@ -5,7 +5,3 @@ variable "region" {
 variable "eks_cluster_name" {
   description = "Name of the EKS cluster"
 }
-
-variable "ecr_name" {
-  description = "Name of the ECR repository"
-}
