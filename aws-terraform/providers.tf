e39c055976ba8f37225233c854b4a0a939205d56
Santiago Barraza <sjbs0212@gmail.com>
2025-06-12 12:21:41 -0500

    chore: change bucket for tfstate

--- aws-terraform/providers.tf
+++ aws-terraform/providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-tfstate-s3-bucket-lsj"
+    bucket         = "tfstatelsj"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
